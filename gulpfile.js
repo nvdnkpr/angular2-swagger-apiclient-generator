cba4e8dcdd6776bd708ba99cfaca4b243a66b9b9
Jonny Tizzard <jmtizzard@msn.com>
2017-04-27 14:21:45 +0100

    Add debug messages; attempt to npm install from git

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
 var gulp = require('gulp');
+var fs = require("fs");
 
-gulp.task('default', ['copyBin','copyLib']);
+gulp.task('default', ['copyBin', 'copyLib']);
 
-gulp.task('copyBin',function(){
-  var stream = gulp.src('src/a2apigen.js').pipe(gulp.dest('bin'));
-  return stream;
+gulp.task('copyBin', function () {
+    var stream = gulp.src('src/a2apigen.js').pipe(gulp.dest('bin'));
+    console.log("copyBin, files present: ", fs.readdirSync("./"));
+    console.log("copyBin, parent files present: ", fs.readdirSync("../"));
+    return stream;
 });
 
-gulp.task('copyLib',function(){
-  var stream = gulp.src('src/generator.js').pipe(gulp.dest('lib'));
-  return stream;
+gulp.task('copyLib', function () {
+    var stream = gulp.src('src/generator.js').pipe(gulp.dest('lib'));
+    console.log("copyLib, files present: ", fs.readdirSync("./"));
+    console.log("copyLib, parent files present: ", fs.readdirSync("../"));
+    return stream;
 });
 
