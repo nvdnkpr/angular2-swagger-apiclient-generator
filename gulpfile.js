9635c6d6ec6b0159c22608fed512482a230fb5af
Jonny Tizzard <jmtizzard@msn.com>
2017-04-27 14:34:36 +0100

    Remove preinstall and debug statements

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,11 @@ gulp.task('default', ['copyBin', 'copyLib']);
 
 gulp.task('copyBin', function () {
     var stream = gulp.src('src/a2apigen.js').pipe(gulp.dest('bin'));
-    console.log("copyBin, files present: ", fs.readdirSync("./"));
-    console.log("copyBin, parent files present: ", fs.readdirSync("../"));
     return stream;
 });
 
 gulp.task('copyLib', function () {
     var stream = gulp.src('src/generator.js').pipe(gulp.dest('lib'));
-    console.log("copyLib, files present: ", fs.readdirSync("./"));
-    console.log("copyLib, parent files present: ", fs.readdirSync("../"));
     return stream;
 });
 
